685b2e41a5cc5a486f1d95900de06754c3859155
Jake Nordin <jacob.nordin4699@gmail.com>
2021-01-30 22:08:29 -0600

    It's Alive!
    Player can move with arrows or A and D and shoot with space bar

--- Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/MovePlayer.cs
+++ Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/MovePlayer.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     public float moveSpeed;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] GameObject gunPos;
+    [SerializeField] float projectileSpeed = 10f;
+    private Rigidbody2D rigBod;
    
     // Start is called before the first frame update
     void Start()
@@ -17,5 +21,15 @@ public class MovePlayer : MonoBehaviour
     {
         
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetKeyDown("space")) 
+        {
+            GameObject bullet = Instantiate(bulletPrefab, gunPos.transform.position,Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
     }
 }
