625630a7af6414bbe767ef8370fd06846f100fb4
Jake Nordin <jacob.nordin4699@gmail.com>
2021-01-30 21:21:49 -0600

    Arrow Key movement and edge boundries

--- Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/MovePlayer.cs
+++ Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/MovePlayer.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    public float moveSpeed;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveSpeed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(pos.x, transform.position.y);
+        
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
     }
 }
