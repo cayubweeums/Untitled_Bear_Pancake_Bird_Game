4c2b8b8a5db60cd5ee513f09b9bb88b59bbe2a0a
Jake Nordin <jacob.nordin4699@gmail.com>
2021-02-03 21:02:33 -0600

    Added artwork

---
+++ Untitled_Bear_Pancake_Bird_Game/Assets/Sprites/CitedArtWork.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitedArtWork : MonoBehaviour
+{
+    // Pancake stack https://pixabay.com/vectors/breakfast-pancakes-stack-syrup-1295968/
+    // Bear https://pixabay.com/illustrations/bear-teddy-teddy-bear-cute-bear-3793241/
+    // bird https://pixy.org/4656854/
+    // rocks https://pixabay.com/vectors/rock-jagged-grey-gray-geology-575598/
+    // background https://pixabay.com/illustrations/field-fields-mountains-landscape-4061730/
+    // basket https://pixabay.com/vectors/basket-picnic-wicker-lunch-304342/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
