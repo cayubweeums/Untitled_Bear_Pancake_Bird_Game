34944df57cb64ba18aef751f08d95588e7c293ba
Caleb <farmingtoncardinals24@gmail.com>
2021-02-03 22:31:32 -0600

    Added Inc score script
    Added a script

--- Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/Inc_Score.cs
+++ Untitled_Bear_Pancake_Bird_Game/Assets/Scripts/Inc_Score.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Despawn_Birds : MonoBehaviour
+public class Inc_Score : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,14 +16,13 @@ public class Despawn_Birds : MonoBehaviour
         
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
-        Debug.Log("Collision");
-
         Destroy(collision.gameObject);
 
         GameManager.Instance.IncScore(1);
     }
+
 
 }
